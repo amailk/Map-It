29c0292568bda4530a4e62a513ec430a4667ad29
Ama Liyanage <liyaniama@gmail.com>
2016-10-30 02:13:56 -0400

    created first marker

--- map.js
+++ map.js
@@ -4,4 +4,11 @@ function initMap() { //initialize js function to load the map
     center: {lat: 43.450301, lng: -80.483192}, //what part of the world to show (lat long literal)
     zoom:13 //higher the more detail. you can go up to level 21
   });
+  var smiletiger = {lat:43.456015, lng:-80.491675};
+
+  var marker = new google.maps.Marker({
+    position: smiletiger,
+    map: map, //map where it should appear on
+    title: 'Smile Tiger Coffee Roasters' //appears when you hover over the Marker
+  });
 }
