cb7939fd6048b5f5d362f4bdab4569a0034ca9b8
Ama Liyanage <liyaniama@gmail.com>
2016-10-31 23:26:54 -0400

    show/hide functions for coffee listings

--- map.js
+++ map.js
@@ -1,48 +1,74 @@
-var map; //map variable
-function initMap() { //initialize js function to load the map
-  map = new google.maps.Map(document.getElementById('map'), { //new map instance, load the map in the map div in the html
-    center: {lat: 43.450301, lng: -80.483192}, //what part of the world to show (lat long literal)
-    zoom:13 //higher the more detail. you can go up to level 21
+//map variable
+var map;
+
+var markers = [];
+
+//initialize js function to load the map
+function initMap() {
+
+  //new map instance, load the map in the map div in the html
+  map = new google.maps.Map(document.getElementById('map'), {
+
+    //what part of the world to show (lat long literal)
+    center: {lat: 43.450301, lng: -80.483192},
+
+    //higher the more detail. you can go up to level 21
+    zoom:13
+
   });
 
-  var smiletiger = {lat:43.456015, lng:-80.491675};
+  var locations = [
+    {title: 'Smile Tiger Coffee Roasters', location: {lat: 43.456015, lng: -80.491675}}
+  ]
 
-  var marker = new google.maps.Marker({
-    position: smiletiger,
-    map: map, //map where it should appear on
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    title: 'Smile Tiger Coffee Roasters' //appears when you hover over the Marker
-  });
+  var largeInfowindow = new google.maps.InfoWindow();
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location;
+    var title = locations[i].title;
 
-  var contentString = '<div id="content">' +
-    '<div id="title">' +
-      '<h1 id="firstheading"><b>Smile Tiger</b></h1>'+
-    '</div>'+
-    '<div id="body-content">'+
-      '<p>'+
-        'Smile Tiger Coffee is a small but fierce shop that roasts' +
-        'seasonal coffee people love.<br>' +
-        'All of our coffees are Direct Trade, transparently sourced,'+
-        'and roasted in small batches. <br>' +
-        'We also offer tiger training to cafes and cubs alike at'+ 'our workshop in Kitchener-Waterloo or at your cafe.'+
-        '</p>'+
-        '</div>'+
-        '</div>';
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+    // keep marker instance in markers array
+    markers.push(marker);
+    marker.addListener('click', function() {
 
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+      //tells infowindow to open at that marker.
+      populateInfoWindow(this, largeInfowindow);
+    });
+  }
+
+  document.getElementById('show-coffee').addEventListener('click', showCoffee);
+  document.getElementById('hide-coffee').addEventListener('click', hideCoffee);
 }
 
-function drop() {
-  for (var i = 0; i < markerArray.length; i++) {
-    setTimeout(function() {
-      addMarkermethod();
-    }, i * 200);
+  function populateInfoWindow(marker, infowindow) {
+    if(infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      infowindow.addListener('closeclick', function() {
+        infowindow.setMarker(null);
+      });
+    }
+  }
+
+function showCoffee() {
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+    bounds.extend(markers[i].position);
+  }
+  map.fitBounds(bounds);
+}
+
+function hideCoffee() {
+  for (var i = 0; i < markers.length; i ++) {
+    markers[i].setMap(null);
   }
 }
