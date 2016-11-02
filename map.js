608ab5d772e36030593bc00aa51ec0b5ebde566c
Ama Liyanage <liyaniama@gmail.com>
2016-11-02 17:40:55 -0400

    additional styles and marker features, cleaned code.

--- map.js
+++ map.js
@@ -8,43 +8,113 @@ function initMap() {
   var styles = [
     {
       featureType: 'water',
-      stylers: [ {color:'#19a0d8'}]
+      stylers: [
+        {color: '#19a0d8'}
+      ]
+    },{
+      featureType: 'administrative',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {color: '#ffffff'},
+        {weight: 6}
+      ]
+    },{
+      featureType: 'administrative',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {color: '#e85113'}
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [
+        {color: '#efe9e4'},
+        {lightness: -40}
+      ]
+    },{
+      featureType: 'transit.station',
+      stylers: [
+        {weight: 9 },
+        {hue: '#e85113'}
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'labels.icon',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    },{
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {lightness: 100}
+      ]
+    },{
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {lightness: -100}
+      ]
+    },{
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [
+        {visibility: 'on'},
+        {color: '#f0e4d3'}
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'geometry.fill',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -25 }
+      ]
     }
-  ]
-  //new map instance, load the map in the map div in the html
+  ];
+
   map = new google.maps.Map(document.getElementById('map'), {
-
-    //what part of the world to show (lat long literal)
     center: {lat: 43.450301, lng: -80.483192},
-
-    //higher the more detail. you can go up to level 21
-    zoom:13
-
+    zoom:13,
+    styles: styles,
+    mapTypeControl: false
   });
 
   var locations = [
-    {title: 'Smile Tiger Coffee Roasters', location: {lat: 43.456015, lng: -80.491675}}
-  ]
+    {title: "Smile Tiger Coffee Roasters", location: {lat: 43.456015, lng: -80.491675}},
+    {title: "Berlin Bicycle Cafe", location: {lat:43.453610, lng:-80.518524}},
+    {title: "Balzac's Kitchener", location: {lat: 43.450938, lng: -80.498264}},
+    {title: "Mercury Coffee", location: {lat: 43.451595, lng: -80.489149}},
+    {title: "Cafe Pyrus", location: {lat: 43.449760, lng: -80.491712}}
+  ];
 
   var largeInfowindow = new google.maps.InfoWindow();
+
+  var defaultIcon = makeMarkerIcon('0091ff');
+
+  var highlightedIcon = makeMarkerIcon('FFFF24');
   for (var i = 0; i < locations.length; i++) {
     var position = locations[i].location;
     var title = locations[i].title;
-
     var marker = new google.maps.Marker({
-      map: map,
       position: position,
       title: title,
+      icon: defaultIcon,
       animation: google.maps.Animation.DROP,
       id: i
     });
 
-    // keep marker instance in markers array
     markers.push(marker);
-    marker.addListener('click', function() {
 
-      //tells infowindow to open at that marker.
+    marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
+    });
+
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
     });
   }
 
@@ -58,7 +128,7 @@ function initMap() {
       infowindow.setContent('<div>' + marker.title + '</div>');
       infowindow.open(map, marker);
       infowindow.addListener('closeclick', function() {
-        infowindow.setMarker(null);
+        infowindow.marker = null;
       });
     }
   }
@@ -76,4 +146,14 @@ function hideCoffee() {
   for (var i = 0; i < markers.length; i ++) {
     markers[i].setMap(null);
   }
+}
+
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21, 34));
+  return markerImage;
 }
